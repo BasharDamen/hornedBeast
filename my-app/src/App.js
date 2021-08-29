5be987db244613b81bc6c63965d6815a694fc638
BasharDamen <bashar.alda@gmail.com>
2021-08-29 22:12:35 +0300

    header

---
+++ my-app/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from './components/header';
+
+
+class App extends React.Component{
+
+  render(){
+    return (<>
+      <Header/>
+    </>
+    )
+    
+  }
+
+}
+
+
+export default App;
