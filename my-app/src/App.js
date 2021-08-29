d8baf7a568b4faa63f052d9923b0e208cbe2c88a
BasharDamen <bashar.alda@gmail.com>
2021-08-29 23:01:05 +0300

    first attempt

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Header from './components/header';
-
+import Main from './components/Main';
+import Footer from './components/Footer';
 
 class App extends React.Component{
 
   render(){
     return (<>
       <Header/>
+      <Main/>
+      <Footer/>
     </>
     )
     
---
+++ my-app/src/components/Main.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Div from './main-components/Div';
+
+class Main extends React.Component{
+
+    render(){
+        return(
+            <>
+            <Div/>
+            <Div/>
+            </>
+        );
+    };
+
+};
+
+export default Main;
