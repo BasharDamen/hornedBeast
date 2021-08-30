6fc1b43767ffa4b349105ff4d48acf5a11cd66f2
BasharDamen <bashar.alda@gmail.com>
2021-08-30 17:24:10 +0300

    lab1

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+class Footer extends React.Component {
+
+render(){
+    return(
+         <>
+            <span>&copy;BasharAldamen</span>
+         </>
+        );
+    };
+};
+
+export default Footer;
